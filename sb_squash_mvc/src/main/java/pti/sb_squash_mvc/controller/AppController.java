92303035e46fbeacc276c3fbece6d31be477662a
Neyrda <adryenn.kope@gmail.com>
2025-03-29 11:47:58 +0100

    correction

--- sb_squash_mvc/src/main/java/pti/sb_squash_mvc/controller/AppController.java
+++ sb_squash_mvc/src/main/java/pti/sb_squash_mvc/controller/AppController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import pti.sb_squash_mvc.service.AppService;
+
 @Controller
 public class AppController {
 	
