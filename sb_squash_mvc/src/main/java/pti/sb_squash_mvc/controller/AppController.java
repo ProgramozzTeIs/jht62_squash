89b1cb2e4f968896c5413fb43c25a5e32ca5f462
kovacsrichard8 <156535601+kovacsrichard8@users.noreply.github.com>
2025-03-29 12:51:37 +0100

    DoLogin

--- sb_squash_mvc/src/main/java/pti/sb_squash_mvc/controller/AppController.java
+++ sb_squash_mvc/src/main/java/pti/sb_squash_mvc/controller/AppController.java
@@ -2,9 +2,10 @@ package pti.sb_squash_mvc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import pti.sb_squash_mvc.service.AppService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AppController {
@@ -21,6 +22,29 @@ public class AppController {
 	public String loginPage() {
 		
 		return "login.html";
+	}
+	
+	@PostMapping("/login")
+	public String doLogin (
+				Model model,
+				@RequestParam("user") String userName,
+				@RequestParam("password") String password
+			
+			) {
+		
+		String targetPage = "";
+		
+		StatusDto statusDto = service.logInUser(userName, password);
+		
+		if(statusDto == null) {
+			
+			targetPage = "login.html";
+		}
+		
+		model.addAttribute("statusdto", statusDto);
+		
+		return targetPage;
+
 	}
 
 }
