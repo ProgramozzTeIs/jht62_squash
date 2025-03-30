acb0de54e9bc47ea34cb33dcae685be3260b041c
Csatti01 <cseh.attila1990@gmail.com>
2025-03-30 03:31:48 +0200

    init StatusDto, UserDto,

--- sb_squash_mvc/src/main/java/pti/sb_squash_mvc/dto/StatusDto.java
+++ sb_squash_mvc/src/main/java/pti/sb_squash_mvc/dto/StatusDto.java
@@ -1,10 +1,26 @@
 package pti.sb_squash_mvc.dto;
 
 public class StatusDto {
+	
+	private boolean error;
+
+	public StatusDto(boolean error) {
+		super();
+		this.error = error;
+	}
+
+	public boolean isError() {
+		return error;
+	}
+
+	public void setError(boolean error) {
+		this.error = error;
+	}
 
 	public boolean getError() {
-		// TODO Auto-generated method stub
 		return false;
 	}
+
+	
 
 }
---
+++ sb_squash_mvc/src/main/java/pti/sb_squash_mvc/dto/UserDto.java
@@ -0,0 +1,30 @@
+package pti.sb_squash_mvc.dto;
+
+public class UserDto {
+	
+	private int userId;
+	private String userName;
+	
+	public UserDto(int userId, String userName) {
+		super();
+		this.userId = userId;
+		this.userName = userName;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+}
